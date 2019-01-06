00cdeee3fef6bb98f8c7336901104fabe567339c
andregcaires <andregcaires@outlook.com>
2019-01-06 16:52:15 -0200

    Implementação Tipo Cliente

---
+++ WebStoreApi/src/main/java/com/andregcaires/webstoreapi/domain/enums/TipoCliente.java
@@ -0,0 +1,39 @@
+package com.andregcaires.webstoreapi.domain.enums;
+
+public enum TipoCliente {
+
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
+	
+	private int cod;
+	private String descricao;
+	
+	
+	// Converte código em enum
+	public static TipoCliente toEnum(Integer cod) {
+		
+		if(cod == null) {
+			return null;
+		}
+		for( TipoCliente x : TipoCliente.values() ) {
+			if( cod.equals(x.getCod()) ) {
+				return x;
+			}
+		}
+		throw new IllegalArgumentException("Id inválido: "+ cod);
+	}
+	
+	private TipoCliente(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+}
