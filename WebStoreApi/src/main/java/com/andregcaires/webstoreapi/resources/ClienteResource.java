16a5ed8115095924ef77c0592ce9787e12d1504c
andre.caires <andre.caires@trust.com.br>
2019-02-04 16:27:21 -0200

    Cliente DTO

--- WebStoreApi/src/main/java/com/andregcaires/webstoreapi/resources/ClienteResource.java
+++ WebStoreApi/src/main/java/com/andregcaires/webstoreapi/resources/ClienteResource.java
@@ -1,15 +1,24 @@
 package com.andregcaires.webstoreapi.resources;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andregcaires.webstoreapi.domain.Cliente;
+import com.andregcaires.webstoreapi.domain.Cliente;
+import com.andregcaires.webstoreapi.dto.ClienteDTO;
 import com.andregcaires.webstoreapi.services.ClienteService;
 
 @RestController
@@ -31,6 +40,49 @@ public class ClienteResource {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente body = service.findById(id);
+		return ResponseEntity.ok().body(body);
+	}
+	
+	@RequestMapping(value = {"/", ""}, method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO obj) {
+		Cliente body = service.insert(service.fromDTO(obj));
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(body.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping( value = {"/{id}", "/{id}/"}, method = RequestMethod.PUT )
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO obj, @PathVariable Integer id) {
+		
+		if(obj.getId() != id) {
+			obj.setId(id);			
+		}
+		
+		Cliente body = service.update(service.fromDTO(obj));
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping( value = {"/{id}"}, method = RequestMethod.DELETE )
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		
+		service.deleteById(id);
+		return ResponseEntity.noContent().build();
+		
+	}
+	
+	
+	@RequestMapping( value = {"/page", "/page/"}, method = RequestMethod.GET )
+	public ResponseEntity<Page<ClienteDTO>> findPage( 
+			@RequestParam(value = "pageIndex", defaultValue = "0") Integer pageIndex
+			, @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage
+			, @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy
+			, @RequestParam(value = "direction", defaultValue = "ASC") String direction ) {
+		
+		Page<ClienteDTO> body = service.findPage(pageIndex, linesPerPage, orderBy, direction).map(obj -> new ClienteDTO(obj));
+
 		return ResponseEntity.ok().body(body);
 	}
 	
--- WebStoreApi/src/main/java/com/andregcaires/webstoreapi/services/ClienteService.java
+++ WebStoreApi/src/main/java/com/andregcaires/webstoreapi/services/ClienteService.java
@@ -4,9 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.andregcaires.webstoreapi.domain.Categoria;
 import com.andregcaires.webstoreapi.domain.Cliente;
+import com.andregcaires.webstoreapi.domain.enums.TipoCliente;
+import com.andregcaires.webstoreapi.dto.CategoriaDTO;
+import com.andregcaires.webstoreapi.dto.ClienteDTO;
 import com.andregcaires.webstoreapi.repositories.ClienteRepository;
 import com.andregcaires.webstoreapi.services.exceptions.ObjectNotFoundException;
 
@@ -32,5 +39,32 @@ public class ClienteService {
 		return _repo.findAll();
 	}
 	
+	public Cliente update(Cliente entity) {
+		Cliente temp = findById(entity.getId());
+		entity.setCpfOuCnpj(temp.getCpfOuCnpj());
+		entity.setTipo(temp.getTipo());
+		return _repo.save(entity);
+	}
+	
+	public Cliente insert(Cliente entity) {
+		
+		entity.setId(null);
+		return _repo.save(entity);
+	}
+	
+	public void deleteById(Integer id) {
+		_repo.deleteById(id);
+	}
+	
+	public Page<Cliente> findPage(Integer pageIndex, Integer linesPerPage, String orderBy, String direction) {		
+		
+		PageRequest pageRequest = PageRequest.of(pageIndex, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return _repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO dto) {
+		return new Cliente(dto.getId(), dto.getNome(), dto.getEmail(), null, null);
+	}
 	
 }
